7b6d0b59ccf6a3ec2d00d617ff5cf3d1a883b297
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-10-29 16:02:39 +0200

    Replaced string concatination with template literals
    I saw that there was a cleaner way to use transform, so I decided to use it.

--- Javascript/mouseReversal.js
+++ Javascript/mouseReversal.js
@@ -12,16 +12,16 @@ const pointer = document.getElementById("pointer");
 const image = document.getElementById("image");
 //grabs hidden image and sets it on the opposite side of the screen as the scam image
 const hidden = document.getElementById("hidden");
-hidden.style.transform = "translate("+(viewportWidth-imgWidth)+"px,"+(viewportHeight-imgHeight)+"px)";
+hidden.style.transform = `translate(${(viewportWidth-imgWidth)}px,${(viewportHeight-imgHeight)}px)`;
 //sets the position of the home link
-document.getElementById("home").style.transform = "translate("+(viewportWidth/2+homeOffset)+"px,"+(viewportHeight/2+homeOffset)+"px)";
+document.getElementById("home").style.transform = `translate(${(viewportWidth/2+homeOffset)}px,${(viewportHeight/2+homeOffset)}px)`;
 //calles mouseController when the real mouse moves around the page 
 document.getElementById("mouseMovement").onmousemove = function(event){
     //finds the coordinates of the real mouse and then calculates where the fake mouse pointer should be translated to
     let pointerx = viewportWidth - event.clientX;
     let pointery = viewportHeight - event.clientY;
     //moves fake pointer to the calculated position
-    pointer.style.transform = "translate("+pointerx+"px,"+pointery+"px)";
+    pointer.style.transform = `translate(${pointerx}px,${pointery}px)`;
 }
 //when your real mouse clicks on the hidden image, this is run
 document.getElementById("hidden").onclick = function(event){
@@ -29,7 +29,7 @@ document.getElementById("hidden").onclick = function(event){
     let imgx = Math.floor(Math.random() * (viewportWidth-imgWidth));
     let imgy = Math.floor(Math.random() * (viewportHeight-imgHeight));
     //translates scam image to those coordinates
-    hidden.style.transform = "translate("+imgx+"px,"+imgy+"px)";
+    hidden.style.transform = `translate(${imgx}px,${imgy}px)`;
     //translates hidden image to coordinates opposit of the scam image
-    image.style.transform = "translate("+(viewportWidth-(imgx+imgWidth))+"px,"+(viewportHeight-(imgy+imgHeight))+"px)";
+    image.style.transform = `translate(${(viewportWidth-(imgx+imgWidth))}px,${(viewportHeight-(imgy+imgHeight))}px)`;
 }
